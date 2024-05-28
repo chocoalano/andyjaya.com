434b5e1f380cad12637440920d19068ff7d3287a
chocoalano <alangentina95@gmail.com>
2024-05-28 17:42:18 +0700

    sadfsadfsadf

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ use Filament\Notifications\Notification;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect('/hrd');
 });
 Route::get('/download-format-import/{id}', [ImportFormatController::class, 'index'])->name('download-format-import');
 Route::get('/slip-gaji/{id}', [Payroll::class, 'slip'])->name('slipgaji.pdf');
