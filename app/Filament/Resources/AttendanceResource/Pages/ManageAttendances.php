da1e807bc05009e10bd381bacb8ddda12a0ba86b
chocoalano <alangentina95@gmail.com>
2024-06-06 11:21:26 +0700

    sdfgdfsgdsfgsdfg

--- app/Filament/Resources/AttendanceResource/Pages/ManageAttendances.php
+++ app/Filament/Resources/AttendanceResource/Pages/ManageAttendances.php
@@ -69,62 +69,70 @@ class ManageAttendances extends ManageRecords
                 return $data;
             })
             ->using(function (array $data): Model {
-                $validateArea = AttArea::whereHas('user', function ($query) use ($data){
-                    $query->where('user_id', $data['user_id']);
-                });
-                $area = $validateArea->count('*');
-                $areaDetail = $validateArea->first();
                 $q = new AttendanceIn();
-                if($area > 0){
-                    $lat1 = $areaDetail->lat;
-                    $lon1 = $areaDetail->lng;
-
-                    // Koordinat Bandung
-                    $lat2 = $data['location']['lat'];
-                    $lon2 = $data['location']['lng'];
-                    $theta = $lon1 - $lon2;
-                    $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-                    $dist = acos($dist);
-                    $dist = rad2deg($dist);
-                    $miles = $dist * 60 * 1.1515;
-                    $meters = round($miles * 1609.344, 2);
-                    if((float)$meters < (float)$areaDetail->radius){
-                        $c = DB::table('att_group_schedule as ags')
-                        ->join('att_times as at','ags.att_time_id','=','at.id')
-                        ->where('ags.id','=', $data['att_group_schedule_id'])
-                        ->select('at.in')
-                        ->first();
-                        $waktu_masuk_seharusnya = new DateTime($c->in);
-                        $waktu_masuk_sebenarnya = new DateTime($data['time']);
-                        $selisih = $waktu_masuk_sebenarnya->diff($waktu_masuk_seharusnya);
-                        $status = strtotime($data['time']) > strtotime($c->in) ?'late':'unlate';
-                        
-                        $q->user_id = $data['user_id'];
-                        $q->att_group_schedule_id = $data['att_group_schedule_id'];
-                        $q->location = $data['location'];
-                        $q->time = $data['time'];
-                        $q->difference = $selisih->format('%H:%I:%S');
-                        $q->photo = $data['photo'];
-                        $q->status = $status;
-                        $q->save();
-                        Notification::make()
-                            ->title('Presence successfuly')
-                            ->success()
-                            ->send();
-                        return $q;
+                if ($data['user_id'] === auth()->id()) {
+                    $validateArea = AttArea::whereHas('user', function ($query) use ($data){
+                        $query->where('user_id', $data['user_id']);
+                    });
+                    $area = $validateArea->count('*');
+                    $areaDetail = $validateArea->first();
+                    if($area > 0){
+                        $lat1 = $areaDetail->lat;
+                        $lon1 = $areaDetail->lng;
+    
+                        // Koordinat Bandung
+                        $lat2 = $data['location']['lat'];
+                        $lon2 = $data['location']['lng'];
+                        $theta = $lon1 - $lon2;
+                        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+                        $dist = acos($dist);
+                        $dist = rad2deg($dist);
+                        $miles = $dist * 60 * 1.1515;
+                        $meters = round($miles * 1609.344, 2);
+                        if((float)$meters < (float)$areaDetail->radius){
+                            $c = DB::table('att_group_schedule as ags')
+                            ->join('att_times as at','ags.att_time_id','=','at.id')
+                            ->where('ags.id','=', $data['att_group_schedule_id'])
+                            ->select('at.in')
+                            ->first();
+                            $waktu_masuk_seharusnya = new DateTime($c->in);
+                            $waktu_masuk_sebenarnya = new DateTime($data['time']);
+                            $selisih = $waktu_masuk_sebenarnya->diff($waktu_masuk_seharusnya);
+                            $status = strtotime($data['time']) > strtotime($c->in) ?'late':'unlate';
+                            
+                            $q->user_id = $data['user_id'];
+                            $q->att_group_schedule_id = $data['att_group_schedule_id'];
+                            $q->location = $data['location'];
+                            $q->time = $data['time'];
+                            $q->difference = $selisih->format('%H:%I:%S');
+                            $q->photo = $data['photo'];
+                            $q->status = $status;
+                            $q->save();
+                            Notification::make()
+                                ->title('Presence successfuly')
+                                ->success()
+                                ->send();
+                            return $q;
+                        }else{
+                            Notification::make()
+                                ->title('The absence radius distance is insufficient, the minimum absence radius distance is '.$areaDetail->radius.' mmeters.')
+                                ->danger()
+                                ->send();
+                                return $q;
+                        }
                     }else{
                         Notification::make()
-                            ->title('The absence radius distance is insufficient, the minimum absence radius distance is '.$areaDetail->radius.' mmeters.')
+                            ->title('Location is not available/invalid location for this user')
                             ->danger()
                             ->send();
                             return $q;
                     }
                 }else{
                     Notification::make()
-                        ->title('Location is not available/invalid location for this user')
-                        ->danger()
-                        ->send();
-                        return $q;
+                            ->title("you can't cheat by excluding other people!")
+                            ->danger()
+                            ->send();
+                            return $q;
                 }
             })
             ->successNotification(null),
