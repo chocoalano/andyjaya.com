a18119243ecdc0caaea8ff7ea933f00f39cc4d68
chocoalano <alangentina95@gmail.com>
2024-06-09 17:28:12 +0700

    sdfgsdg

--- app/Filament/Resources/PayrollResource.php
+++ app/Filament/Resources/PayrollResource.php
@@ -67,6 +67,7 @@ class PayrollResource extends Resource implements HasShieldPermissions
                                 $userGroup = UserAttGroup::whereHas('userTeams', function ($query) use ($state) {
                                     $query->where('user_id', $state);
                                 })->first();
+                                dd($userGroup);
                                 $dateArray = explode(" - ", $periode);
                                 $date1 = DateTime::createFromFormat('d/m/Y', $dateArray[0])->format('Y-m-d');
                                 $date2 = DateTime::createFromFormat('d/m/Y', $dateArray[1])->format('Y-m-d');
